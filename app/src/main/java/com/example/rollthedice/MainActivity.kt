1c433d139d08c6a2d37ac6a2fabd3164a49994b1
raffaeleGraz <raffaele.graziani06@gmail.com>
2025-04-01 13:35:51 +0200

    modifiche prof.Amendola

---
+++ app/src/main/java/com/example/rollthedice/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.rollthedice
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        var btnRoll = findViewById<Button>(R.id.buttonRoll)
+        var imgRoll = findViewById<ImageView>(R.id.imageViewDice)
+        var txt = findViewById<TextView>(R.id.textViewTitle)
+
+        btnRoll.setOnClickListener(View.OnClickListener {
+            var mioToast = Toast.makeText(this, "DADO LANCIATO!!!", Toast.LENGTH_SHORT)
+            mioToast.show()
+        })
+    }
+}
