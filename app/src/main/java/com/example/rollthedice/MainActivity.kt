757a189bcafb406ad6c81690b85b2dfcbff9c113
raffaeleGraz <raffaele.graziani06@gmail.com>
2025-04-23 17:41:29 +0200

    Aggiunta di: - pulsante di avvio - funzione per estrarre un numero random - funzione per passare al secondo activity main - array associativo con le relative immagini dei dadi - visualizzazione del dado corretto

--- app/src/main/java/com/example/rollthedice/MainActivity.kt
+++ app/src/main/java/com/example/rollthedice/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.rollthedice
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Random
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +15,20 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val startBtn = findViewById<Button>(R.id.startBtn)
+        startBtn.setOnClickListener{
+            var numero = estrazione()
+            intent(numero)
+        }
+    }
+
+    private fun estrazione():Int {
+        return (1..6).random()
+    }
+
+    private fun intent(numero:Int){
+        var intent = Intent(this, MainActivity2::class.java)
+        intent.putExtra("numero", numero)
+        startActivity(intent)
     }
 }
